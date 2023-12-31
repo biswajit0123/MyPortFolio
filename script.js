d7235f31d23df71c92dc885831cca0bd5b7f713d
biswajit0123 <biswajitmuduli0544@gmail.com>
2023-12-31 14:24:38 +0530

    file adding

---
+++ script.js
@@ -0,0 +1,16 @@
+var HamBurgerMenu=document.getElementById('menu');
+var CrossSymbol=document.getElementById('cross');
+var DropMenu=document.getElementById('dpmn');
+
+ function openMenu(){
+DropMenu.style.display="block";
+HamBurgerMenu.style.display="none";
+CrossSymbol.style.display="block";
+
+ }
+
+ function closeMenu(){
+    DropMenu.style.display="none";
+    HamBurgerMenu.style.display="block";
+    CrossSymbol.style.display="none";
+     }
